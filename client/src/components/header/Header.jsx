c8ddd9d2e7765e2c61f29de8ffca57fe39b69b1c
Anton <32438821+AntonNordgren@users.noreply.github.com>
2025-04-26 23:05:15 +0200

    Worked on the navbar

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,9 +3,20 @@ import React from 'react'
 import './headerStyle.css'
 
 function Header() {
+
+  const headerOptions = ["About", "Races", "Units", "Lore"]
+
   return (
-    <div className="header">
-      Header
+    <div className="header d-flex p-3">
+      <div className="headerLogoContainer">
+        <img className="headerLogoImg h-100" src="../images/logotype.png" />
+      </div>
+      <div className="optionlistContainer d-flex h-100">
+        {headerOptions.map(option =>
+        <div className="headerOption my-auto mx-2 align-middle">
+          <h3 className="m-0">{option}</h3>
+        </div>)}
+      </div>
     </div>
   )
 }
